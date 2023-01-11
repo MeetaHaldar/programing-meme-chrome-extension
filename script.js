b340d526b03cc0adfe54c3950d21c9c4972ff986
meeta haldar <meetahaldar1001@gmail.com>
2023-01-12 00:02:36 +0530

    changing api

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let image = document.getElementById("image");
 
-fetch("https://meme-api.herokuapp.com/gimme/ProgrammerHumor", {
+fetch("https://meme-api.com/gimme/ProgrammerHumor", {
   method: "GET",
 })
   .then((data) => data.json())
   .then((result) => {
     image.src = result.url;
+    console.log(result);
   })
   .catch((err) => {
     console.error(err);
